313622bac1e750cdd46bac21b8ce817c18a12afa
JABirchall <JABirchall@users.noreply.github.com>
2019-03-07 14:57:02 +0000

    add Annoucement config

---
+++ Nimda/Configuration/Core/Announcement.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Nimda\Configuration\Core;
+
+
+class Announcement
+{
+    public static $config = [
+        'message' => "I'm a core announcement in configured in ". __FILE__,
+        'interval' => 10,
+        'once' => false,
+        'guildId' => '544112985980010496',
+        'channelId' => '544112985980010498',
+    ];
+}
