ad7eab223e07492e64aa3fc2b8740d050ce63c89
Jake Birchall <JABirchall@users.noreply.github.com>
2019-05-19 21:03:18 +0100

    fix instances where avatar is not set

--- Nimda/Core/Timers/SetPresence.php
+++ Nimda/Core/Timers/SetPresence.php
@@ -3,8 +3,11 @@
 namespace Nimda\Core\Timers;
 
 use CharlotteDunois\Yasmin\Client;
+use CharlotteDunois\Yasmin\Models\ClientUser;
+use Doctrine\Instantiator\Exception\InvalidArgumentException;
 use Nimda\Core\Timer;
 use React\Promise\PromiseInterface;
+use function React\Promise\reject;
 
 class SetPresence extends Timer
 {
@@ -13,8 +16,11 @@ class SetPresence extends Timer
      */
     public function trigger(Client $client): PromiseInterface
     {
-        return $client->user->setAvatar($this->config['avatar'])->then(function () use ($client) {
-            return $client->user->setPresence($this->config['presence']);
+        return $client->user->setPresence($this->config['presence'])->then(function (ClientUser $clientUser) {
+            if($this->config['avatar'] === '') {
+                return reject(new InvalidArgumentException('Avatar not set'));
+            }
+            return $clientUser->client->user->setAvatar($this->config['avatar']);
         });
     }
 }
